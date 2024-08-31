25cfe850ca5584f11d7fc34061b8e3b8d8506f17
wcoding <papa5944@gmail.com>
2024-08-31 22:59:48 +0900

    make cart quatity interactive

--- script/products.js
+++ script/products.js
@@ -56,46 +56,78 @@ products.forEach(product =>{
 				<p>${product.name}</p>
 				<p>${product.rating}</p>
 				<h2>$${product.price}</h2>
-				<select name='qty' style='padding:.4rem .9rem;'>
-						<option value=''>1</option>
-						<option value=''>2</option>
-						<option value=''>3</option>
-						<option value=''>4</option>
+
+				<select style='padding:.4rem .9rem'; class='select-option-${product.id}'>
+						<option value='1'>1</option>
+						<option value='2'>2</option>
+						<option value='3'>3</option>
+						<option value='4'>4</option>
 				</select>
 				<button type='button' name='button' data-product-item='${product.id}' id='cartButton'>Add to cart</button>
+
 		</div>
 		`
 });
 
-document.querySelector('.image-layout').innerHTML = productsHTML;
+		document.querySelector('.image-layout').innerHTML = productsHTML;
 
-	 document.querySelectorAll('#cartButton').forEach((button) =>{
+		//Add to cart function
+		function addToCart(button){
 
-			button.addEventListener('click', ()=>{
+			let selectValue = document.querySelector(`.select-option-${button.dataset.productItem}`).value
+			let updateQty = Number(selectValue);
+			const productId = Number(button.dataset.productItem);
+			let matchItem;
 
-				const productId = Number(button.dataset.productItem);
-				let matchItem;
+			cart.forEach((item) =>{
+				if(productId=== item.id){
+						matchItem = item;
+				}
+		});
 
-				cart.forEach((item) =>{
-					if(productId=== item.id){
-							matchItem = item;
-					}
-			});
-
-			if(matchItem){
-				matchItem.qty += 1;
-			}else{
-
+		if(matchItem){
+			matchItem.qty += updateQty ;
+		}else{
 				cart.push({
 					id: productId,
-					qty:1,
+					qty:updateQty,
 				});
-			}
-			let count = 0;
-			cart.forEach(item=>{
-				count += item.qty;
-			})
-			document.querySelector('.cart-qty').textContent = count;
+		}
+
+	}
+
+//update cart quatity
+function updateCart(){
+	let count = 0;
+	cart.forEach(cartItem =>{
+		count += cartItem.qty
+	})
+	document.querySelector('.cart-qty').textContent = count;
+}
+
+	function addMessage(){
+			setTimeout(()=>{
+					document.querySelector('.add-message').style.display= 'block';
+		},1000)
+
+	}
+
+	function removeMessage(){
+			setTimeout(()=>{
+					document.querySelector('.add-message').style.display= 'none';
+		},3000)
+
+	}
+
+removeMessage()
+
+	document.querySelectorAll('#cartButton').forEach((button) =>{
+
+			button.addEventListener('click', ()=>{
+				addToCart(button);
+				updateCart();
+				addMessage();
+				removeMessage()
 
 		});
 });
